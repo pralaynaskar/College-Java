3720082be9005ecfe46a641bcf598e3f5861ad83
Pralay Naskar <143413651+pralaynaskar@users.noreply.github.com>
2023-10-07 21:53:40 +0530

    Update and rename Fibbonacci upto n.java to 4-Fibbonacci upto n.java

--- 4-Fibbonacci upto n.java
+++ 4-Fibbonacci upto n.java
@@ -1,9 +1,11 @@
-
-class Fibonacci {
-    public static void main(String[] args) {
+class Fibonacci
+{
+    public static void main(String[] args) 
+    {
         int n = 20, prev = 0, current = 1;
         System.out.println("Fibonacci Series up to " + n + ":");
-        while (prev <= n) {
+        while (prev <= n) 
+        {
             System.out.print(prev + " ");
                 int next = prev + current;
                 prev = current;
